1e1a75621dff4f765769863ca7893917fafdd02d
Andhika Bagas <87473932+Kadigas@users.noreply.github.com>
2024-10-10 11:00:38 +0700

    feat: admin button on home

--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -67,15 +67,17 @@
 				<li class="mr-3">
 					<a class="inline-block text-white no-underline hover:text-gray-200 hover:text-underline py-2 px-4 {{ Request::is('staffdisplay') ? 'active': '';}}" href="{{ route('staffDisplay') }}">Staff</a>
 				</li>
-				{{-- <li class="mr-3">
+				<li class="mr-3">
 					@auth
-					<a href="/logout" type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Logout</a>
+						@if(Auth::user()->role == 'admin')
+							<a href="/admin" type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Admin</a>
+						@endif
 					@endauth
-					@guest
+					{{-- @guest
 					<a href="/auth" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">myITS SSO</a>
-					@endguest
+					@endguest --}}
 					</form>
-				</li> --}}
+				</li>
 			</ul>
 		</div>
 	</nav>
